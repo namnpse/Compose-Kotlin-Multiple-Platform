7bb2f760e9276340f63773d6f751b8fee1bfa0fb
namnpse <nam444479@gmail.com>
2024-07-15 00:20:03 +0900

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/com/namnpse/cmp_kmp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.namnpse.cmp_kmp
+
+import App
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
