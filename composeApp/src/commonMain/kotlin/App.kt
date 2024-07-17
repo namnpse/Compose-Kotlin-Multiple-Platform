04fe9bd06410d88d46a69ed9ce5b14c258383ae1
namnpse <nam444479@gmail.com>
2024-07-17 23:14:15 +0900

    expect/actual implementation for BatteryManager, display battery for each platform

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,5 @@
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -9,18 +10,22 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    batteryManager: BatteryManager
+) {
     MaterialTheme {
         val greetingMessage = Greeting.greet()
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize(),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = greetingMessage
             )
-
+            Text(
+                text = "The current battery level is ${batteryManager.getBatteryLevel()}"
+            )
         }
     }
 }
